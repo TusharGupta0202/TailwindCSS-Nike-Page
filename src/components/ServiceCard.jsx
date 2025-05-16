92bbc0d5bc4963c529093c81cabe1993748e4e40
Tushar Gupta <t.gupta0202@gmail.com>
2025-05-16 20:27:29 +0530

    buided projec

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 
 const ServiceCard = ({imgURL,label,subtext}) => {
   return (
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,5 @@
 import { services } from '../constants'
-import ServiceCard from '../components/Servicecard'
+import ServiceCard from '../components/ServiceCard'
 
 const Services = () => {
   return (
